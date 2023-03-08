fb33ecba963989729f30cb0497731e772d5a0143
vigarp <vigar.8421@outlook.com>
2023-03-08 21:25:05 +0700

    create appCarousel

--- templates/home/HomePageTemplate.tsx
+++ templates/home/HomePageTemplate.tsx
@@ -1,3 +1,4 @@
+import { Skeleton } from "@mantine/core";
 import dynamic from "next/dynamic";
 
 // components
@@ -8,12 +9,39 @@ const AppContainer = dynamic(
   () => import("@components/atoms/containers/AppContainer"),
   { ssr: false }
 );
+const AppCarousel = dynamic(
+  () => import("@components/organisms/carousels/AppCarousel"),
+  { ssr: false, loading: () => <Skeleton height={400} width="100%" my="md" /> }
+);
 
 const HomePageTemplate: React.FC = () => {
+  const dataBanner = [
+    {
+      id: 1,
+      link: "#",
+      url: "https://res.cloudinary.com/vigarp/image/upload/v1678283862/samples/banner1_klna66.jpg",
+      width: 900,
+      height: 300,
+    },
+    {
+      id: 2,
+      link: "#",
+      url: "https://res.cloudinary.com/vigarp/image/upload/v1678283863/samples/banner2_b1cxi8.jpg",
+      width: 900,
+      height: 300,
+    },
+    {
+      id: 3,
+      link: "#",
+      url: "https://res.cloudinary.com/vigarp/image/upload/v1678283862/samples/banner3_kkrrxo.jpg",
+      width: 900,
+      height: 300,
+    },
+  ];
   return (
     <Layout>
       <AppContainer mih="150vh" size="xl" py="xl">
-        <div>b</div>
+        <AppCarousel content={dataBanner} />
       </AppContainer>
     </Layout>
   );
